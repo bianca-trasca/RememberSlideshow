f702a9cd9c1d3653244c41cfe9260e3b9709d75d
Bianca Trasca <97734998+bianca-trasca@users.noreply.github.com>
2022-06-20 10:18:50 +0000

    Changed UI again.

--- Remember/dashboard.Designer.cs
+++ Remember/dashboard.Designer.cs
@@ -50,9 +50,9 @@
             // 
             // createSL
             // 
-            this.createSL.Location = new System.Drawing.Point(598, 577);
+            this.createSL.Location = new System.Drawing.Point(887, 407);
             this.createSL.Name = "createSL";
-            this.createSL.Size = new System.Drawing.Size(105, 23);
+            this.createSL.Size = new System.Drawing.Size(105, 25);
             this.createSL.TabIndex = 1;
             this.createSL.Text = "Create SlideShow";
             this.createSL.UseVisualStyleBackColor = true;
@@ -60,9 +60,9 @@
             // 
             // browseBtn
             // 
-            this.browseBtn.Location = new System.Drawing.Point(125, 577);
+            this.browseBtn.Location = new System.Drawing.Point(196, 407);
             this.browseBtn.Name = "browseBtn";
-            this.browseBtn.Size = new System.Drawing.Size(60, 23);
+            this.browseBtn.Size = new System.Drawing.Size(90, 25);
             this.browseBtn.TabIndex = 2;
             this.browseBtn.Text = "Browse";
             this.toolTip1.SetToolTip(this.browseBtn, "Click to browse photos");
@@ -75,19 +75,19 @@
             this.listBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.listBox.ForeColor = System.Drawing.SystemColors.Info;
             this.listBox.FormattingEnabled = true;
+            this.listBox.HorizontalScrollbar = true;
             this.listBox.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.listBox.Location = new System.Drawing.Point(3, 401);
+            this.listBox.Location = new System.Drawing.Point(8, 7);
             this.listBox.Name = "listBox";
-            this.listBox.ScrollAlwaysVisible = true;
-            this.listBox.Size = new System.Drawing.Size(700, 171);
+            this.listBox.Size = new System.Drawing.Size(278, 392);
             this.listBox.TabIndex = 3;
             this.listBox.SelectedIndexChanged += new System.EventHandler(this.ListBox_SelectedIndexChanged);
             // 
             // clearBtn
             // 
-            this.clearBtn.Location = new System.Drawing.Point(64, 577);
+            this.clearBtn.Location = new System.Drawing.Point(102, 407);
             this.clearBtn.Name = "clearBtn";
-            this.clearBtn.Size = new System.Drawing.Size(60, 23);
+            this.clearBtn.Size = new System.Drawing.Size(90, 25);
             this.clearBtn.TabIndex = 4;
             this.clearBtn.Text = "Clear list";
             this.toolTip1.SetToolTip(this.clearBtn, "Click to clear all photos");
@@ -96,9 +96,9 @@
             // 
             // deleteBtn
             // 
-            this.deleteBtn.Location = new System.Drawing.Point(3, 577);
+            this.deleteBtn.Location = new System.Drawing.Point(8, 407);
             this.deleteBtn.Name = "deleteBtn";
-            this.deleteBtn.Size = new System.Drawing.Size(60, 23);
+            this.deleteBtn.Size = new System.Drawing.Size(90, 25);
             this.deleteBtn.TabIndex = 5;
             this.deleteBtn.Text = "Delete";
             this.toolTip1.SetToolTip(this.deleteBtn, "Click to delete the selected photo");
@@ -109,7 +109,7 @@
             // 
             this.pictureBox.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox.Location = new System.Drawing.Point(292, 7);
             this.pictureBox.Margin = new System.Windows.Forms.Padding(10);
             this.pictureBox.Name = "pictureBox";
             this.pictureBox.Size = new System.Drawing.Size(700, 392);
@@ -124,9 +124,9 @@
             this.closeMusicBtn.FlatAppearance.BorderSize = 0;
             this.closeMusicBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.closeMusicBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.closeMusicBtn.Location = new System.Drawing.Point(280, 579);
+            this.closeMusicBtn.Location = new System.Drawing.Point(388, 407);
             this.closeMusicBtn.Name = "closeMusicBtn";
-            this.closeMusicBtn.Size = new System.Drawing.Size(20, 20);
+            this.closeMusicBtn.Size = new System.Drawing.Size(25, 25);
             this.closeMusicBtn.TabIndex = 14;
             this.closeMusicBtn.Text = "X";
             this.toolTip1.SetToolTip(this.closeMusicBtn, "Click to remove selected music");
@@ -135,9 +135,9 @@
             // 
             // musicBtn
             // 
-            this.musicBtn.Location = new System.Drawing.Point(194, 577);
+            this.musicBtn.Location = new System.Drawing.Point(292, 407);
             this.musicBtn.Name = "musicBtn";
-            this.musicBtn.Size = new System.Drawing.Size(83, 23);
+            this.musicBtn.Size = new System.Drawing.Size(90, 25);
             this.musicBtn.TabIndex = 12;
             this.musicBtn.Text = "Add music";
             this.toolTip1.SetToolTip(this.musicBtn, "Click to browse music");
@@ -150,7 +150,7 @@
             this.musicLbl.BackColor = System.Drawing.Color.Transparent;
             this.musicLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.musicLbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.musicLbl.Location = new System.Drawing.Point(303, 581);
+            this.musicLbl.Location = new System.Drawing.Point(419, 413);
             this.musicLbl.Name = "musicLbl";
             this.musicLbl.Size = new System.Drawing.Size(0, 13);
             this.musicLbl.TabIndex = 13;
@@ -161,7 +161,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(705, 603);
+            this.ClientSize = new System.Drawing.Size(999, 438);
             this.Controls.Add(this.closeMusicBtn);
             this.Controls.Add(this.musicLbl);
             this.Controls.Add(this.musicBtn);
